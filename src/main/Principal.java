65d4f437ed8bab38c7dadfa4d62c02b86f99a94d
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2021-09-13 23:51:31 -0300

    Endereço e Fornecedor adicionados no Menu

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -8,13 +8,19 @@ import java.util.Scanner;
 
 import entidade.Cliente;
 import entidade.Endereco;
+import entidade.Fornecedor;
 import servico.EnderecoServico;
+import servico.FornecedorServico;
 import entidade.Produto;
 import servico.ClienteServico;
 import servico.ProdutoServico;
 
 public class Principal {
 
+	static EnderecoServico enderecoServico = new EnderecoServico();
+	
+	static FornecedorServico fornecedorServico = new FornecedorServico();
+	
 	static ProdutoServico produtoServico = new ProdutoServico();
 
 	static ClienteServico clienteServico = new ClienteServico();
@@ -22,11 +28,11 @@ public class Principal {
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String args[]) throws ParseException {
-		int escolha = 5;
+		int escolha = 6;
 
 		while (true) {
 
-			if (escolha == 5) {
+			if (escolha == 6) {
 				escolha = menuPrincipal();
 			}
 
@@ -39,25 +45,37 @@ public class Principal {
 			case 1:
 				menuProduto();
 				break;
+			case 2:
+				menuFornecedor();
+				break;
 			case 3:
 				menuCliente();
 				break;
+			case 4:
+				menuEndereço();
+				break;
+			/*
+			 * case 5:
+				menuPedido();
+				break;
+			 */
 			default:
 				System.out.println("Opção inválida!");
 				break;
 			}
 
-			escolha = 5;
+			escolha = 6;
 		}
 	}
 
 	public static int menuPrincipal() {
 
 		System.out.println("Escolha uma das opções a seguir:");
-		System.out.println("(1) Cadastro de Produto");
-		System.out.println("(2) Cadastro de Fornecedor");
-		System.out.println("(3) Cadastro de Cliente");
-		System.out.println("(4) Realizar Pedido");
+		System.out.println("(1) Produto");
+		System.out.println("(2) Fornecedor");
+		System.out.println("(3) Cliente");
+		System.out.println("(4) Endereço");
+		System.out.println("(5) Realizar Pedido");
 		System.out.println("(0) Sair");
 
 		int escolha = scanner.nextInt();
@@ -67,7 +85,7 @@ public class Principal {
 
 	public static int menuCadastro() {
 
-		System.out.println("Escolha uma das opções a seguir:");
+		System.out.println("O que você deseja fazer?");
 		System.out.println("(1) Cadastrar");
 		System.out.println("(2) Editar");
 		System.out.println("(3) Remover");
@@ -164,11 +182,11 @@ public class Principal {
 		}
 
 		System.out.println();
-		System.out.println("Digite o id do produto para confirmar");
+		System.out.println("Digite o ID do produto para confirmar");
 
 		int idProduto = scanner.nextInt();
 		scanner.nextLine();
-		System.out.println("id informado: " + idProduto);
+		System.out.println("ID informado: " + idProduto);
 
 		System.out.println("Digite as novas informações do produto");
 
@@ -195,7 +213,7 @@ public class Principal {
 			System.out.println(produto);
 		}
 
-		System.out.println("Digite o id do produto para confirmar");
+		System.out.println("Digite o ID do produto para confirmar");
 
 		int idProduto = scanner.nextInt();
 
@@ -303,11 +321,11 @@ public class Principal {
 		}
 
 		System.out.println();
-		System.out.println("Digite o id do cliente para confirmar");
+		System.out.println("Digite o ID do cliente para confirmar");
 
 		int idCliente = scanner.nextInt();
 		scanner.nextLine();
-		System.out.println("id informado: " + idCliente);
+		System.out.println(" informado: " + idCliente);
 
 		System.out.println("Digite as novas informações do cliente");
 
@@ -334,7 +352,7 @@ public class Principal {
 			System.out.println(cliente);
 		}
 
-		System.out.println("Digite o id do cliente para confirmar");
+		System.out.println("Digite o ID do cliente para confirmar");
 
 		int idCliente = scanner.nextInt();
 
@@ -357,6 +375,303 @@ public class Principal {
 		
 		System.out.println();
 	}
+	
+	
+	
+	
+	
+	// FORNECEDOR
+	
+	public static void menuFornecedor() throws ParseException {
+
+		switch (menuCadastro()) {
+		case 1:
+			cadastraFornecedor();
+			break;
+		case 2:
+			editaFornecedor();
+			break;
+		case 3:
+			removeFornecedor();
+			break;
+		case 4:
+			consultaFornecedor();
+			break;
+		case 5:
+			break;
+		default:
+			System.out.println("Opção inválida!");
+			break;
+		}
+	}
+	
+	
+	
+	public static Fornecedor infoFornecedor() throws ParseException {
+		Fornecedor fornecedor = new Fornecedor();
+
+		System.out.println("Nome do Fornecedor:");
+		String nomeFornecedor = scanner.nextLine();
+		System.out.println("Nome informado: " + nomeFornecedor);
+
+		System.out.println("Email:");
+		String email = scanner.nextLine();
+		System.out.println("Email informado: " + email);
+
+		System.out.println("Telefone:");
+		String telefone = scanner.nextLine();
+		System.out.println("Telefone informado: " + telefone);
+
+		fornecedor.setNomeFornecedor(nomeFornecedor);
+		fornecedor.setEmail(email);
+		fornecedor.setTelefone(telefone);
+		
+		return fornecedor;
+	}
+
+	public static void cadastraFornecedor() throws ParseException {
+
+		System.out.println("Digite as informações do fornecedor");
+		scanner.nextLine();
+
+		Fornecedor fornecedor = infoFornecedor();
+
+		int idFornecedor = fornecedorServico.inserirFornecedor(fornecedor);
+
+		String msgFornecedorInserido = "Fornecedor " + fornecedor.getNome() + " inserido com sucesso!";
+
+		if (idFornecedor > 0) {
+			System.out.println(msgFornecedorInserido);
+			System.out.println();
+		} else {
+			System.out.println("Erro ao inserir fornecedor");
+		}
+	}
+
+	public static void editaFornecedor() throws ParseException {
+
+		System.out.println("Digite o nome do fornecedor que deseja editar");
+
+		scanner.nextLine();
+		String nome = scanner.nextLine();
+
+		List<Fornecedor> fornecedores = fornecedorServico.buscarFornecedorPorNome(nome);
+
+		for (Fornecedor fornecedor : fornecedores) {
+			System.out.println(fornecedor);
+		}
+
+		System.out.println();
+		System.out.println("Digite o ID do fornecedor para confirmar");
+
+		int idFornecedor = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("ID informado: " + idFornecedor);
+
+		System.out.println("Digite as novas informações do fornecedor");
+
+		Fornecedor fornecedor = infoFornecedor();
+
+		fornecedor.setId(idFornecedor);
+
+		String msgFornecedorAtualizado = fornecedorServico.alterarFornecedor(fornecedor);
+
+		System.out.println(msgFornecedorAtualizado);
+		System.out.println();
+
+	}
+
+	public static void removeFornecedor() {
+		System.out.println("Digite o nome do fornecedor que deseja remover");
+		scanner.nextLine();
+
+		String nome = scanner.nextLine();
+
+		List<Fornecedor> fornecedores = fornecedorServico.buscarFornecedorPorNome(nome);
+
+		for (Fornecedor fornecedor: fornecedores) {
+			System.out.println(fornecedor);
+		}
+
+		System.out.println("Digite o ID do fornecedor para confirmar");
+
+		int idFornecedor = scanner.nextInt();
+
+		Fornecedor fornecedor = new Fornecedor();
+
+		fornecedor.setId(idFornecedor);
+		String msgFornecedorExcluido = fornecedorServico.excluirFornecedor(fornecedor);
+
+		System.out.println(msgFornecedorExcluido);
+		System.out.println();
+	}
+
+	public static void consultaFornecedor() {
+
+		List<Fornecedor> todosFornecedores = fornecedorServico.buscarTodosFornecedores();
+
+		for (Fornecedor fornecedor: todosFornecedores) {
+			System.out.println(fornecedor);
+		}
+		
+		System.out.println();
+	}
+	
+	
+	// ENDEREÇO
+	
+	public static void menuEndereço() throws ParseException {
+
+		switch (menuCadastro()) {
+		case 1:
+			cadastraEndereço();
+			break;
+		case 2:
+			editaEndereço();
+			break;
+		case 3:
+			removeEndereço();
+			break;
+		case 4:
+			consultaEndereço();
+			break;
+		case 5:
+			break;
+		default:
+			System.out.println("Opção inválida!");
+			break;
+		}
+	}
+	
+	
+	
+	public static Endereco infoEndereço() throws ParseException {
+		Endereco endereco = new Endereco();
+		
+		
+		System.out.println("Informe as informações do endereço:");
+		scanner.nextLine();
+		System.out.println("Cidade:");
+		String cidade = scanner.nextLine();
+		System.out.println("Cidade informada: " + cidade);
+
+		System.out.println("CEP:");
+		String cep = scanner.nextLine();
+		System.out.println("CEP informado: " + cep);
+
+		System.out.println("Estado:");
+		String estado = scanner.nextLine();
+		System.out.println("Estado informado: " + estado);
+		
+		System.out.println("Rua:");
+		String rua = scanner.nextLine();
+		System.out.println("Rua informada: " + rua);
+		
+		System.out.println("Bairro:");
+		String bairro = scanner.nextLine();
+		System.out.println("Bairro informado: " + bairro);
+		
+		System.out.println("Número:");
+		int numero = scanner.nextInt();
+		System.out.println("Número informado: " + numero);
+		
+
+		endereco.setCidade(cidade);
+		endereco.setCep(cep);
+		endereco.setEstado(estado);
+		endereco.setRua(rua);
+		endereco.setBairro(bairro);
+		endereco.setNumero(numero);
+		
+		return endereco;
+	}
+
+	public static void cadastraEndereço() throws ParseException {
+
+		Endereco endereco = infoEndereço();
+
+		int idEndereco = enderecoServico.inserirEndereco(endereco);
+		
+		String msgEndereçoInserido = "Endereço inserido com sucesso!";
+
+		if (idEndereco > 0) {
+			System.out.println(msgEndereçoInserido);
+			System.out.println();
+		} else {
+			System.out.println("Erro ao inserir endereço");
+		}
+	}
+
+	public static void editaEndereço() throws ParseException {
+
+		System.out.println("Digite o endereço que deseja editar pela cidade:");
+
+		scanner.nextLine();
+		String cidade = scanner.nextLine();
+
+		List<Endereco> enderecos = enderecoServico.buscarEnderecoPorCidade(cidade);
+
+		for (Endereco endereco : enderecos) {
+			System.out.println(endereco);
+		}
+
+		System.out.println();
+		System.out.println("Digite o ID do endereço para confirmar");
+
+		int idEndereco = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("ID informado: " + idEndereco);
+
+		System.out.println("Digite as novas informações do endereco");
+
+		Endereco endereco = infoEndereço();
+
+		endereco.setId(idEndereco);
+
+		String msgFornecedorAtualizado = enderecoServico.atualizarEndereco(endereco);
+
+		System.out.println(msgFornecedorAtualizado);
+		System.out.println();
+
+	}
+
+	public static void removeEndereço() {
+		System.out.println("Digite o nome do endereço que deseja remover");
+		scanner.nextLine();
+
+		String cidade = scanner.nextLine();
+
+		List<Endereco> enderecos = enderecoServico.buscarEnderecoPorCidade(cidade);
+
+		for (Endereco endereco: enderecos) {
+			System.out.println(endereco);
+		}
+
+		System.out.println("Digite o ID do endereço para confirmar");
+
+		int idEndereco = scanner.nextInt();
+
+		Endereco endereco = new Endereco();
+
+		endereco.setId(idEndereco);
+		String msgEnderecoExcluido = enderecoServico.excluirEndereco(endereco);
+
+		System.out.println(msgEnderecoExcluido);
+		System.out.println();
+	}
+
+	public static void consultaEndereço() {
+
+		List<Endereco> todosEnderecos = enderecoServico.buscarTodosEnderecos();
+
+		for (Endereco endereco: todosEnderecos) {
+			System.out.println(endereco);
+		}
+		
+		System.out.println();
+	}
+	
+	
 }
 
 /*
